18c591a1f00ce86b0e91cbf92dca9327dbe90587
Hamza1610 <muhammadhamza162003@gmail.com>
2024-12-22 00:44:20 -0800

    add firebase initialize app

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -7,11 +7,13 @@ import { getStorage } from 'firebase/storage';
 console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 
 const firebaseConfig = {
-  authDomain: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 

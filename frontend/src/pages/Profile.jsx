79c2f51f707ef9f39c35cbd59b1f9d8eb421f24f
Hamza1610 <muhammadhamza162003@gmail.com>
2024-12-22 00:50:51 -0800

    create attendance page

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='attendance'>
+      
+      <h2>Lecturer/Teacher profile</h2>
+      <p>Profile content</p>
+    </div>
+  )
+}
+export default Profile;

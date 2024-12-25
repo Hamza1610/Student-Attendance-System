250fed6c0361b2ec13df1012cfdf1f15888a5a24
Hamza1610 <muhammadhamza162003@gmail.com>
2024-12-25 08:42:44 -0800

    add firebase-auth profile fetch

--- backend/app/api/endpoints/auth.py
+++ backend/app/api/endpoints/auth.py
@@ -1,7 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from firebase_admin import auth
 
 router = APIRouter()
 
-@router.get('/')
-def get_auth_state(user_token: str): # since firebase is used to auth token is needed from user
-    pass
+@router.get("/api/auth/me")
+async def get_profile(user_id: str):
+    try:
+        # Fetch user from Firebase Authentication
+        user = auth.get_user(user_id)
+        if not user:
+            return {
+                "error": "User is not verified"
+            }
+        
+        return {
+            "uid": user.uid,
+            "displayName": user.display_name,
+            "email": user.email,
+            "photoURL": user.photo_url,
+            "emailVerified": user.email_verified,
+            "phoneNumber": user.phone_number,
+        }
+    except Exception as e:
+        return {"error": str(e)}

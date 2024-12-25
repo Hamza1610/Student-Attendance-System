6feea5248f4e96025eccfb1742a593ccd5f00a39
Hamza1610 <muhammadhamza162003@gmail.com>
2024-12-25 08:51:00 -0800

    centralise import

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,5 @@
+# Desc: Initialize all models for the application
+from .student_model import StudentModel
+from .class_model import ClassModel
+from .attendance_model import AttendanceModel
+from .face_recognition_model import FaceRecognitionModel

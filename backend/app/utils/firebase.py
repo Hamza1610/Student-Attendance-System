cc2c759d852a3a4db04e4fea9e8b10d4872fa98b
Hamza1610 <muhammadhamza162003@gmail.com>
2025-01-01 11:12:17 -0800

    modify helper functions and firebase auth

--- backend/app/utils/firebase.py
+++ backend/app/utils/firebase.py
@@ -6,10 +6,13 @@ load_dotenv()
 # Path to your service account key file
 SERVICE_ACCOUNT_PATH =os.environ.get('FIREBASE_ADMIN_PATH')
 
-if not SERVICE_ACCOUNT_PATH:
-    raise ValueError("No FIREBASE_ADMIN_PATH set for firebase admin sdk")
 
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate(SERVICE_ACCOUNT_PATH)
-firebase_app = firebase_admin.initialize_app(cred)
+def initalize_firebase_admin():
 
+    if not SERVICE_ACCOUNT_PATH:
+        raise ValueError("No FIREBASE_ADMIN_PATH set for firebase admin sdk")
+
+    print(SERVICE_ACCOUNT_PATH)
+    # Initialize Firebase Admin SDK
+    cred = credentials.Certificate(SERVICE_ACCOUNT_PATH)
+    firebase_admin.initialize_app(cred)
--- backend/app/utils/helpers.py
+++ backend/app/utils/helpers.py
@@ -1,15 +1,31 @@
+import pickle
 import base64
 
-def encode_binary_data(data):
-    if isinstance(data, bytes):
-        return base64.b64encode(data).decode('utf-8')  # Convert binary to Base64 string
-    return data
+def convert_embedding_to_base64(embedding):
 
-from bson import ObjectId
+    """
+    Converts a list of embeddings to a base64 encoded string.
+    """
+    binary_data = pickle.dumps(embedding)  # Serialize to binary
+    return base64.b64encode(binary_data).decode('utf-8')  # Encode as base64 string
 
-def serialize_mongo_data(data):
-    if isinstance(data, list):
-        return [{**item, "_id": str(item["_id"])} for item in data]
-    elif isinstance(data, dict):
-        return {**data, "_id": str(data["_id"])}
-    return data
+
+
+def convert_base64_to_embedding(base64_string):
+
+    """
+    Decodes a base64 string back into the original embedding list.
+    """
+    binary_data = base64.b64decode(base64_string)  # Decode from base64
+    return pickle.loads(binary_data)  # Deserialize from binary
+
+
+
+# Test
+# emedding = [2,3,34, 43.2, -22, 4]
+
+# _e_b = convert_embedding_to_base64(emedding)
+# _b_e = convert_base64_to_embedding(_e_b)
+
+# print(_e_b)
+# print(_b_e)

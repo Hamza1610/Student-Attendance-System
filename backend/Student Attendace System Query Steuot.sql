0ff98a6fccd2c14bee2c2934117f0b6d5c841e6b
Hamza1610 <muhammadhamza162003@gmail.com>
2025-01-10 21:58:59 +0100

    modify tables structure qsql uery

--- backend/Student Attendace System Query Steuot.sql
+++ backend/Student Attendace System Query Steuot.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS attendance;
 
 CREATE TABLE attendance (
     id SERIAL PRIMARY KEY,
-    class_id VARCHAR NOT NULL,
+    class_name VARCHAR NOT NULL,
     date DATE NOT NULL,
     student_id VARCHAR UNIQUE NOT NULL,
     status VARCHAR NOT NULL
@@ -14,7 +14,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Create the 'users' table
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id SERIAL PRIMARY KEY,
+    user_id VARCHAR UNIQUE NOT NULL,
     name VARCHAR NOT NULL,
     email VARCHAR UNIQUE NOT NULL,
     role VARCHAR NOT NULL,
@@ -23,23 +24,23 @@ CREATE TABLE IF NOT EXISTS users (
 
 -- Create the 'classes' table
 CREATE TABLE IF NOT EXISTS classes (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id SERIAL PRIMARY KEY,
     name VARCHAR NOT NULL,
     description TEXT,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
     status VARCHAR DEFAULT 'active',
-    coordinator_id VARCHAR NOT NULL, -- this can give conflict base on the use of if it's uuid or email
-    FOREIGN KEY (coordinator_id) REFERENCES users(email) ON DELETE SET NULL --  this is also affected
+    teacher_id VARCHAR NOT NULL, -- this can give conflict base on the use of if it's uuid or email
+    FOREIGN KEY (teacher_id) REFERENCES users(user_id) ON DELETE SET NULL --  this is also affected
 );
 
 -- Create the 'students' table
 CREATE TABLE IF NOT EXISTS students (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id SERIAL PRIMARY KEY,
     name VARCHAR NOT NULL,
     student_id VARCHAR UNIQUE NOT NULL,
     email VARCHAR UNIQUE NOT NULL,
-    class_id VARCHAR,
+    class_name VARCHAR NOT NULL,
     face_embedding BYTEA,  -- Use BYTEA for storing BLOB data in PostgreSQL
-    registered_by VARCHAR
+    registered_by VARCHAR NOT NULL
 );

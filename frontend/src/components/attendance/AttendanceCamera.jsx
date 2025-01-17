ea51377a9686c90473c27e34a8fbdb9fba61196f
Hamza1610 <muhammadhamza162003@gmail.com>
2025-01-17 23:50:09 +0100

    feat: add face recognition attendance component with webcam integration

---
+++ frontend/src/components/attendance/AttendanceCamera.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useRef, useState } from "react";
+import * as faceapi from "face-api.js";
+import Webcam from "react-webcam";
+import '../../styles/AttendanceCamera.css'
+
+const FaceRecognitionAttendance = () => {
+  const webcamRef = useRef(null);
+  const [modelsLoaded, setModelsLoaded] = useState(false);
+  const [detections, setDetections] = useState([]);
+
+  // Load face-api.js models
+  useEffect(() => {
+    const loadModels = async () => {
+      const MODEL_URL = "/models"; // Ensure models are in the public/models directory
+      await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
+      setModelsLoaded(true);
+    };
+    loadModels();
+  }, []);
+
+  // Perform face detection
+  const detectFaces = async () => {
+    if (
+      webcamRef.current &&
+      webcamRef.current.video.readyState === 4 &&
+      modelsLoaded
+    ) {
+      const video = webcamRef.current.video;
+      const detections = await faceapi
+        .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceDescriptors();
+      setDetections(detections); // Store detected faces
+    }
+  };
+
+  const handleCapture = () => {
+    if (detections && detections.length > 0) {
+      console.log(detections['0']);
+      return
+    }
+    return
+  }
+
+  useEffect(() => {
+    const interval = setInterval(detectFaces, 100); // Detect faces every 100ms
+    return () => clearInterval(interval);
+  }, [modelsLoaded]);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <div style={{ position: "relative", display: 'inline-block' }}>
+          <Webcam
+            ref={webcamRef}
+            videoConstraints={{
+              width: 600,
+              height: 400,
+              facingMode: "user",
+            }}
+            style={{
+              borderRadius: "8px",
+              border: "2px solid #ccc"
+            }}
+          />
+          {detections.map((detection, index) => (
+            <div
+              key={index}
+              style={{
+                position: "absolute",
+                border: "2px solid red",
+                left: detection.detection.box.x,
+                top: detection.detection.box.y,
+                width: detection.detection.box.width,
+                height: detection.detection.box.height,
+              }}
+            />
+          ))}
+          {detections.length > 0 && <p>{detections.length}st face(s) detected!</p>}
+          <button className="camera-btn" onClick={handleCapture}>Record attendance</button>
+      </div>
+    </div>
+  );
+};
+
+export default FaceRecognitionAttendance;
---
+++ frontend/src/contexts/AttendanceContext.jsx
@@ -0,0 +1,59 @@
+import React, { createContext, useState, useContext } from 'react';
+import apiClient from '../services/api';
+import { getUserIdFromCookie } from '../services/auth.service';
+
+
+// Create AttendanceContext
+const AttendanceContext = createContext();
+
+// Provider component
+export const AttendanceProvider = ({ children }) => {
+  const [classes, setClasses] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  // Fetch classes
+  const fetchClasses = async () => {
+    try {
+      const id = getUserIdFromCookie()
+      const response = await apiClient.get(`/api/classes/${id}`) // fetch with id
+      const classes_data  = response.data.classes
+      console.log(classes_data);
+      
+      if (classes_data.length === 0) {
+        setError('No classes found.');
+      }
+
+      setClasses(classes_data);
+    } catch (err) {
+      setError(`Failed to fetch classes. ${err}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Update student attendance
+  const updateStudentAttendance = async (id, updatedData) => {
+    try {
+        // to implement api call later
+        await apiClient.put(`/api/classes/${id}`, updatedData);
+
+    } catch (error){
+        // update table
+        setError('Failed to update student attendance');
+        console.log(error);
+        
+    }
+  };
+
+
+
+  return (
+    <AttendanceContext.Provider value={{ classes, loading, error, fetchClasses, updateStudentAttendance }}>
+      {children}
+    </AttendanceContext.Provider>
+  );
+};
+
+// Custom hook to use context
+export const useAttendance = () => useContext(AttendanceContext);

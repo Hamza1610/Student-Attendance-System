b5251600f46c601938d549af72718e96a5205c9a
Hamza1610 <muhammadhamza162003@gmail.com>
2025-01-23 23:48:17 +0100

    refactor logic for auth check

--- frontend/src/components/auth/PrivateRoute.jsx
+++ frontend/src/components/auth/PrivateRoute.jsx
@@ -3,12 +3,12 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-    // const { currentUser } = useAuth();
+    const { currentUser } = useAuth();
 
     // THis will be impimented when other fearures are done
-    // if (!currentUser) {
-    //     return <Navigate to="/login" />;
-    // }
+    if (!currentUser) {
+        return <Navigate to="/login" />;
+    }
 
     return <component {...rest} />;
 };
